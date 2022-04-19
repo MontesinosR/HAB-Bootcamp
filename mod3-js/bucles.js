a7b895e4b1260c4612789a5ddcd0f7b95b48a877
Carlos Montesinos <carlosmontesinos1990@gmail.com>
2022-04-19 23:38:28 +0100

    bucles

---
+++ mod3-js/bucles.js
@@ -0,0 +1,35 @@
+"use strict";
+
+// let a = 0;
+
+// while (a < 5) {
+//   console.log(a);
+
+//   a++;
+// }
+
+for (let a = 0; a < 5; a++) {
+  console.log(a);
+
+  if (a === 3) {
+    break;
+  }
+}
+
+// let gameOver = false;
+
+// while (gameOver === false) {
+//   // Lógica del juego. Llegaría un punto donde el usuario muere y gameOver pasa a true
+// }
+
+// for (let a = 1; a <= 5; a++) {
+//   for (let b = 1; b <= 5; b++) {
+//     console.log(a, b);
+//   }
+// }
+
+// const nombre = "Pepito";
+
+// for (let i = 0; i < nombre.length; i++) {
+//   console.log(nombre[i]);
+// }
