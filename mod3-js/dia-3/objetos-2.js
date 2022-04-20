dfa0cfc6eccd58c52c68a7c4b15205fae6e08f57
Carlos Montesinos <carlosmontesinos1990@gmail.com>
2022-04-20 11:34:53 +0100

    objetos-2

---
+++ mod3-js/dia-3/objetos-2.js
@@ -0,0 +1,48 @@
+"use strict";
+
+const usuario = {
+  nombre: "pepe",
+  apellido: "fernandez",
+  edad: 38,
+  direcciones: [
+    {
+      ciudad: "La Coruña",
+      calle: "Perico de los palotes",
+    },
+    {
+      ciudad: "Málaga",
+      calle: "Cualquier plaza guay",
+    },
+  ],
+};
+
+// Acceder a la ciudad de la segunda dirección de pepe
+
+// console.log(usuario.direcciones[1].ciudad);
+
+// for (const direccion of usuario.direcciones) {
+//   console.log(direccion.ciudad);
+// }
+
+// OPTIONAL CHAINING
+
+const usuarios = [
+  {
+    nombre: "pepe",
+    apellido: "fernandez",
+    edad: 38,
+    direccion: {
+      ciudad: "La Coruña",
+      calle: "Plaza Pontevedra",
+    },
+  },
+  {
+    nombre: "pepe",
+    apellido: "fernandez",
+    edad: 38,
+  },
+];
+
+for (const usuario of usuarios) {
+  console.log(usuario.direccion?.ciudad);
+}
