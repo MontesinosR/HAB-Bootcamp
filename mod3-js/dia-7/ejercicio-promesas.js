9c8951295bc40b950ec845ab9275d387f3974fd3
Carlos Montesinos <carlosmontesinos1990@gmail.com>
2022-04-27 11:51:10 +0100

    ejercicios

--- mod3-js/dia-7/ejercicio-promesas.js
+++ mod3-js/dia-7/ejercicio-promesas.js
@@ -1,27 +1,3 @@
-/**
- *
- * Dados estos arrays de usuarios y emails necesitamos que se ejecuten
- * varias tareas.
- *
- * 1. Crear una función llamada getUser que reciba un
- * argumento nombre, busque a ese usuario y devuelva una
- * Promise. Si encuentra el user, debe resolverse con
- * los datos del usuario, si no, reject con un error de
- * que el usuario no existe
- *
- * 2. Crear una función getMail que recibe el id de un
- * usuario y retorna una Promise. Si encuentra el usuario,
- * debe resolverse con el email, si no, debe hacer reject
- * con un error de que usuario no exite
- *
- * NOTA: Dentro de las Promesas usar un setTimeout de 2 segundos para
- * simular el tiempo de respuesta de la búsqueda.
- *
- * BONUS: Una vez acabes las dos funciones, y compruebes su funcionamiento, haz
- * que el resolve de la Promise que devuelve getUser, en vez de devolver el usuario,
- * devuelva su email. Debes llamar a la función getEmail() y pasarle el id del usuario encontrado
- *
- * */
 
 const users = [
   { id: 1, name: "Pedro" },
@@ -33,6 +9,71 @@ const emails = [
   { id: 2, email: "laura@gmail.com" },
 ];
 
-const getUser = (nombre) => {
-  // Retornar promesa que se resuelve con el usuario encontrado, en caso de que exista, o que se rechace con un error, en caso de que no
+/* Dados estos arrays de usuarios y emails necesitamos que se ejecuten
+ varias tareas.
+
+ 1. Crear una función llamada getUser que reciba un
+ argumento nombre, busque a ese usuario y devuelva una
+ Promise. Si encuentra el user, debe resolverse con
+ los datos del usuario, si no, reject con un error de
+ que el usuario no existe */
+
+const getUser = (name) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const userFound = users.find((user) => {
+        return user.name === name;
+      });
+
+      if (userFound) {
+        resolve(userFound);
+      } else {
+        reject(new Error("Usuario no encontrado"));
+      }
+    }, 2000);
+  });
 };
+
+getUser("Pedro")
+  .then((user) => {
+    console.log(user);
+  })
+  .catch((error) => {
+    console.error(error.message);
+  });
+
+  /* 2. Crear una función getMail que recibe el id de un
+ usuario y retorna una Promise. Si encuentra el usuario,
+ debe resolverse con el email, si no, debe hacer reject
+ con un error de que usuario no exite */
+
+ const getEmail = (id) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const emailFound = emails.find((email) => {
+        return email.id === id;
+      });
+
+      if (emailFound) {
+        resolve(emailFound);
+      } else {
+        reject(new Error("Email no encontrado"));
+      }
+    }, 2000);
+  });
+};
+
+getEmail(2)
+  .then((email) => {
+    console.log(email);
+  })
+  .catch((error) => {
+    console.error(error.message);
+  });
+
+ /* NOTA: Dentro de las Promesas usar un setTimeout de 2 segundos para
+ simular el tiempo de respuesta de la búsqueda.
+
+ BONUS: Una vez acabes las dos funciones, y compruebes su funcionamiento, haz
+ que el resolve de la Promise que devuelve getUser, en vez de devolver el usuario,
+ devuelva su email. Debes llamar a la función getEmail() y pasarle el id del usuario encontrado. */
